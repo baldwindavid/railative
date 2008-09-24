628cb491388d4440d01b1f417bc20f3956ec6efd
David Baldwin <baldwindavid@gmail.com>
2008-09-23 20:13:05 -0400

    docs for helpers

--- lib/bilson/railative/helpers.rb
+++ lib/bilson/railative/helpers.rb
@@ -65,11 +65,17 @@ module Bilson
           times['after'][:options] << o
         end
 
+        # iterate over before and after
         times.each do |key, side|
+          # iterate through all items of each side
           side[:options].collect! do |item|
 
+            # if the item is an array, this has an overriden label
+            # Example ['My overriden label!', 1.year]
             if item.is_a?(Array)
-              label_override = item[0]          
+              # set the label value to the first item in this array => 'My overriden label!'
+              label_override = item[0]
+              # item is either a time interval or a string representation of a time interval          
               item = item[1]
               item_representation = nil
             else
@@ -78,25 +84,42 @@ module Bilson
               item_representation = nil
             end
 
+            # if the item is a String than we know that the time select values will go into the
+            # database as interval representations
             if item.is_a?(String)
-              raise 'Please enter a :median_time Time object' unless options[:median_time].is_a?(Time)
+              # clean up the passed string time interval representation for database
               item_representation = railative_interval_to_db(item, :prepend_negative => (side[:operator] == '-'))
-              item_label = railative_interval_to_pretty(item)          
-              calculated_time = options[:median_time].add_railative_interval(item, :prepend_negative => (side[:operator] == '-'))
+              # take the hardcoded interval representation and pretty it up for display
+              # 1.year, 3.months, 4.days becomes 1 year, 3 months, 4 days
+              item_label = railative_interval_to_pretty(item)
+              # if we have a median time to work with, we can calculate an exact time relative to the median
+              # in this case, this is only useful if we are displaying the time in the label
+              if options[:median_time].is_a?(Time)          
+                calculated_time = options[:median_time].add_railative_interval(item, :prepend_negative => (side[:operator] == '-'))
+              end
             end
 
+            # LABEL SETUP
 
-
+            # if the label is overriden then set the label to the overriden value
+            # ['My Overriden Label!', 1.year] => 'My Overriden Label!'
             if !label_override.blank?
               label = label_override
-            else     
+            else
+              # start out with either the :before_prefix or :after_prefix
               label = options["#{key}_prefix".to_sym]
 
+              # if there is no timestamp format entered or if we do want the interval and timestamp in the label
               if options[:label_timestamp_format].blank? || options[:interval_and_timestamp_label]
 
+                # if we have an item_representation then we know the label value is already prettified
                 if !item_representation.blank?
                   label = label + item_label
                 else
+                  # if the time interval is not hardcoded as a String and is less than one day, Rails
+                  # will show a string representation in seconds rather than hours or minutes
+                  # we need to make sure it shows 3 hours instead of 10800 seconds
+                  # Note: This is only for one value (i.e. 1.hour rather than 1.hour, 12.minutes)
                   if item.seconds < 86400
                     label = label + case item.seconds
                       when 0..59   then "#{item.seconds} seconds"
@@ -107,38 +130,66 @@ module Bilson
                       when 7200..86399 then "#{item.seconds / 3600} hours"
                     end
                   else
+                    # Rails will print anything over a day in pretty format by default
+                    # 1.day => 1 day
                     label = label + item.inspect
                   end
                 end
 
+                # if we the time interval and timestamp are combined we need to be able 
+                # to plant text in between the two values
+                # Example: if you set :before_interval_suffix => 'before' you would get something like: 
+                # 1 year before => 12/25/2008
                 if options[:interval_and_timestamp_label]
                   label = label + options["#{key}_interval_suffix".to_sym]
                 end
               end
 
+              # if a timestamp format is entered (:rfc, :long, etc) OR we want a mixed label and 
+              # the median time is a Time object
               if (!options[:label_timestamp_format].blank? || options[:interval_and_timestamp_label]) && options[:median_time].is_a?(Time)
+                
+                # if we want a mixed label
                 if options[:interval_and_timestamp_label]
+                  # option to change the divider between the interval and timestamp
+                  # default is ' => '
                   label = label + options[:interval_and_timestamp_label_divider]
+                  # option to add a prefix to the timestamp portion of the label
+                  # Example: :before_timestamp_prefix => ' around ' might produce:
+                  # 1 year => around 12/25/2008
                   label = label + options["#{key}_timestamp_prefix".to_sym]
                 end
 
+                # if we have an item_representation then a String representation of the time
+                # interval was in the array
                 if !item_representation.blank?
+                  # we calculated the time from this string representation above
                   label = label + calculated_time.to_s(options[:label_timestamp_format])
                 else
+                  # calculate the time - add or subtract depending upon whether this is before or after
+                  # we can also format timestamp
                   label = label + options[:median_time].send(side[:operator], item).to_s(options[:label_timestamp_format])
                 end
               end
 
+              # add a suffix to the label
               label = label + options["#{key}_suffix".to_sym]
 
             end
 
 
+            # VALUE SETUP
 
+            # if we have an item_representation then a String representation of the time
+            # interval was in the array
             if !item_representation.blank?
+              # this String representation was cleaned up for database entry above
+              # values will be 1.year, 2.months, etc.
               value = item_representation
             else
+              # if the median_time is a Time object...
               if options[:median_time].is_a?(Time)
+                # if a specific timestamp format is entered (:rfc, :long, etc)
                 if !options[:value_timestamp_format].blank?
                   value = (options[:median_time].send(side[:operator], item)).to_s(options[:value_timestamp_format])
                 else
@@ -152,6 +203,7 @@ module Bilson
                 end
               else
                 value = (side[:operator] == '-' ? -item : item).to_i
+                value = options[:median_time].class.to_s
               end
             end
 
@@ -161,6 +213,7 @@ module Bilson
         end
 
 
+        # MEDIAN TIME SETUP
 
         if options[:median_time].is_a?(Time) && !options[:value_timestamp_format].blank?
           median_time_option = [ options[:median_time_label], options[:median_time].to_s(options[:value_timestamp_format]) ]
